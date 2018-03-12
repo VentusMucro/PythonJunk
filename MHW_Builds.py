c080ce3092288a5e1cb0242344b6435a788eeaf6
VentusMucro <pc.cornwell90+git@gmail.com>
2018-03-12 15:29:49 -0500

    updated to store pin to file

--- MHW_Builds.py
+++ MHW_Builds.py
@@ -1,12 +1,18 @@
+import os
+
+PATH_TO_PIN = os.path.expanduser('~\Documents\MHW_PIN.txt')
+file = None
+BUILD_PIN = None
 continue_choice = None
 end_flag = False
-BUILD_PIN = 1717
 
-pin_check = input("Enter PIN\n")
-if int(pin_check) == BUILD_PIN:
-	while end_flag != True:
+startup(PATH_TO_PIN)
+
+pin_check = input("Enter PIN\n") #App secured by pin, pin is defined on first use
+if int(pin_check) == BUILD_PIN: #Check pin
+	while end_flag != True: #Begin program
 		build_to_search = input("What build do you want?\n")
-		if build_to_search == "exit":
+		if build_to_search == "exit": #Exit program
 			print("Goodbye.")
 			end_flag = True
 			continue
@@ -17,7 +23,27 @@ else:
 
 
 
-#Section for functions
+#Functions
+def startup(path):
+	BUILD_PIN = get_pin(path)
+	file.close()
+
+def get_pin(path):
+	file = open(path, 'r')
+	if not file:
+		file = open(path, 'w')
+		new_pin = input("What would you like your PIN to be?")
+		file.write(new_pin)
+		return new_pin
+	else:
+		return file.read()
+
+def run_comparison(search_term, comparator): #For potential future use, hope to store build names to a file for looping and less code duplication
+	if search_term == comparator:
+		print("")
+		end_flag = ask_continue()
+		#Need to finish this if/when file system is in place
+
 def ask_continue():
 	continue_choice = input("Search for another?(y/n)\n")
 	if continue_choice[0].lower == 'y':
