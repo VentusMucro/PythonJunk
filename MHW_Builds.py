083c06852daf4512ec1ac8ee85a4a0b97438488d
VentusMucro <pc.cornwell90+git@gmail.com>
2018-03-12 16:04:57 -0500

    corrected storing/pulling pin from file

--- MHW_Builds.py
+++ MHW_Builds.py
@@ -1,43 +1,24 @@
 import os
-
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 PATH_TO_PIN = os.path.expanduser('~\Documents\MHW_PIN.txt')
 file = None
-BUILD_PIN = None
 continue_choice = None
 end_flag = False
-
-startup(PATH_TO_PIN)
-
-pin_check = input("Enter PIN\n") #App secured by pin, pin is defined on first use
-if int(pin_check) == BUILD_PIN: #Check pin
-	while end_flag != True: #Begin program
-		build_to_search = input("What build do you want?\n")
-		if build_to_search == "exit": #Exit program
-			print("Goodbye.")
-			end_flag = True
-			continue
-else:
-	print("Incorrect PIN. Goodbye.")
-
-
-
-
-
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 #Functions
-def startup(path):
-	BUILD_PIN = get_pin(path)
-	file.close()
-
 def get_pin(path):
-	file = open(path, 'r')
-	if not file:
+	if os.path.isfile(path):
+		file = open(path, 'r')
+		to_return = file.read()
+		file.close()
+		return to_return
+	else:
 		file = open(path, 'w')
 		new_pin = input("What would you like your PIN to be?")
 		file.write(new_pin)
+		file.close()
 		return new_pin
-	else:
-		return file.read()
-
+		
 def run_comparison(search_term, comparator): #For potential future use, hope to store build names to a file for looping and less code duplication
 	if search_term == comparator:
 		print("")
@@ -50,3 +31,17 @@ def ask_continue():
 		return False
 	else:
 		return True
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#Program
+BUILD_PIN = int(get_pin(PATH_TO_PIN).strip())
+
+pin_check = input("Enter PIN\n") #App secured by pin, pin is defined on first use
+if int(pin_check) == BUILD_PIN: #Check pin
+	while end_flag != True: #Begin program
+		build_to_search = input("What build do you want?\n")
+		if build_to_search == "exit": #Exit program
+			print("Goodbye.")
+			end_flag = True
+			continue
+else:
+	print("Incorrect PIN. Goodbye.")
